c43f5870845f3b1def688358998c105663c7a6f5
akihide-hatano <a.h.19841202@gmail.com>
2025-09-15 14:33:17 +0900

    checkNumber.test.jsを作成。

---
+++ checkNumber.test.js
@@ -0,0 +1,19 @@
+const checkNumber = require('./count');
+
+describe('checkNumber', () => {
+    test('15以上の数値が入力された場合、正しいメッセージを返す', () => {
+        expect(checkNumber('20')).toBe('入力された数字は20で、15以上です');
+    });
+
+    test('15未満の数値が入力された場合、正しいメッセージを返す', () => {
+        expect(checkNumber('10')).toBe('入力された数字は10で、15未満です');
+    });
+
+    test('数値に変換できない文字列が入力された場合、エラーメッセージを返す', () => {
+        expect(checkNumber('hello')).toBe('無効な文字列です');
+    });
+
+    test('空の文字列が入力された場合、エラーメッセージを返す', () => {
+        expect(checkNumber('')).toBe('無効な文字列です');
+    });
+});
